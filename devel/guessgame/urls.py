e3e2b7e18f14279c8b8439cdf713be72005d7f90
Matt Rodriguez <mattr@sdsc.edu>
2011-08-29 22:02:20 -0700

    Added the application specific urls.py file to the project.

---
+++ devel/guessgame/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls.defaults import patterns, include, url
+
+urlpatterns = patterns('guessgame.views',
+              (r'^$', 'index'),
+              (r'^(?P<game_id>\d+)/$', 'detail'),
+              (r'^(?P<game_id>\d+)/guess/$', 'pick'),
+              (r'^(?P<game_id>\d+)/results/$', 'results'),
+              (r'newgame/$', 'newgame'),
+              )
+              
