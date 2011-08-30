03a1b1e76f8d64e15e7ffddf51a0cd84f0d4caa4
Matt Rodriguez <mattr@sdsc.edu>
2011-08-29 22:06:53 -0700

    Removed unused import

--- devel/guessgame/views.py
+++ devel/guessgame/views.py
@@ -1,7 +1,7 @@
 # Create your views here.
 import random
 import datetime
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
